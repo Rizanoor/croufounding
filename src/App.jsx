f66d5ff478011c85e2c544c9f1aedefc8d6ac427
Rizanoor <81269453+Rizanoor@users.noreply.github.com>
2023-09-12 20:48:37 +0700

    feat: init vite react tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="w-screen h-screen bg-red-300">
+        <h1 className="text-center text-6xl"> Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
